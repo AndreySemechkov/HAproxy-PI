ed4442e71b4a083ae235b3939d77c4deaf499efb
JenyaKapul <37771786+JenyaKapul@users.noreply.github.com>
2018-03-25 15:35:13 +0300

    prime algo implementation

---
+++ WebService/Prime Algo.py
@@ -0,0 +1,15 @@
+import time
+import math
+
+def closestPrime(num):
+    for i in range(num, 1, -1):
+        isPrime = True
+        for j in range(2,int(math.ceil(math.sqrt(i)))):
+            if i % j == 0:
+                isPrime = False
+            if isPrime:
+                return i
+
+start = time.clock()
+print closestPrime(123456)
+print time.clock() - start
