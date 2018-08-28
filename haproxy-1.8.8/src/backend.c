f616d6d8707d7ca0e929811b037db287b66b789c
AndreySemechkov <andrey.semechkov@gmail.com>
2018-08-28 18:57:26 +0300

    debug prints to log added

--- haproxy-1.8.8/src/backend.c
+++ haproxy-1.8.8/src/backend.c
@@ -614,14 +614,15 @@ int assign_server(struct stream *s)
 			break;
 
 		case BE_LB_LKUP_PITREE:
-			if(!srv->proxy->lbprm.pi.log)
-				srv->proxy->lbprm.pi.log = fopen("~/HAproxy-PI/pi_log", "a");
+			/*if(!srv->proxy->lbprm.pi.log)
+				srv->proxy->lbprm.pi.log = fopen("~/HAproxy-PI/pi_log", "a");*/
 			srv = pi_get_next_server(s->be, prev_srv);
+			/*
 			if(srv)
 				fprintf(srv->proxy->lbprm.pi.log, "Started backend: assign_server BE_LB_LKUP_PITREE case\n"
 												"server pointer: %d\n"
 												"num connections of srv: %d\n",
-						srv,srv->cur_sess);
+						srv,srv->cur_sess);*/
 			break;
 
 		case BE_LB_LKUP_CHTREE:
--- haproxy-1.8.8/src/lb_pi.c
+++ haproxy-1.8.8/src/lb_pi.c
@@ -232,8 +232,8 @@ static void pi_update_server_weight(struct server *srv)
  */
 void pi_init_server_tree(struct proxy *p)
 {
-    p->lbprm.pi.log = fopen("~/HAproxy-PI/pi_log", "a");
-    fprintf(p->lbprm.pi.log, "Started pi_init_server_tree\n");
+    //p->lbprm.pi.log = fopen("~/HAproxy-PI/pi_log", "a");
+    //fprintf(p->lbprm.pi.log, "Started pi_init_server_tree\n");
     struct server *srv;
     struct eb_root init_head = EB_ROOT;
     p->lbprm.pi.last_used_node = NULL;
@@ -272,9 +272,9 @@ struct server *pi_get_next_server(struct proxy *p, struct server *srvtoavoid)
     struct server *srv, *avoided;
     struct eb32_node *node;
     //p->lbprm.pi.last_used_node = NULL;
-    fprintf(p->lbprm.pi.log, "Started pi_get_next_server p->lbprm.pi.last_used_node = %d\n", p->lbprm.pi.last_used_node);
-    if(p->lbprm.pi.last_used_node)
-        fprintf(p->lbprm.pi.log, "pi_get_next_server p->lbprm.pi.last_used_node.key = %d", p->lbprm.pi.last_used_node->key);
+    //fprintf(p->lbprm.pi.log, "Started pi_get_next_server p->lbprm.pi.last_used_node = %d\n", p->lbprm.pi.last_used_node);
+    /*if(p->lbprm.pi.last_used_node)
+        fprintf(p->lbprm.pi.log, "pi_get_next_server p->lbprm.pi.last_used_node.key = %d", p->lbprm.pi.last_used_node->key);*/
     //TODO: instead static add last_used_node to struct proxy as a new struct pi_metadata for pi logic
             // add if(last_used_node==deleted_serv) last_used_node = NULL;
                     // check in all code when server dies, do last_used_node =NULL to select here the next best cadidate
@@ -301,8 +301,8 @@ struct server *pi_get_next_server(struct proxy *p, struct server *srvtoavoid)
         srv = NULL;
         goto out;
     }
-    if(node)
-        fprintf(p->lbprm.pi.log, "pi_get_next_server chosen node key = %d", node->key);
+    /*if(node)
+        fprintf(p->lbprm.pi.log, "pi_get_next_server chosen node key = %d", node->key);*/
     while (node) {
         /*
         OK, we have a server. However, it may be saturated, in which
@@ -321,20 +321,21 @@ struct server *pi_get_next_server(struct proxy *p, struct server *srvtoavoid)
         }
         node = eb32_next(node);
     }
-    if(node)
-        fprintf(p->lbprm.pi.log, "pi_get_next_server chosen node after saturation switch key = %d", node->key);
+    /*if(node)
+        fprintf(p->lbprm.pi.log, "pi_get_next_server chosen node after saturation switch key = %d", node->key);*/
 //TODO  incase avoided used should last_used_node be (srv==avoided).lb_node or just NULL to avoid bug of going to avoided server with last used.
     // if last_used_node= NULL its like last_used_node died and we take the next best candidate
-    if(srv)
-        fprintf(p->lbprm.pi.log, "pi_get_next_server chosen server before avoided switch currsess= %d", srv->cur_sess);
+    /*if(srv)
+        fprintf(p->lbprm.pi.log, "pi_get_next_server chosen server before avoided switch currsess= %d", srv->cur_sess);*/
     if (!srv)
         srv = avoided;
     p->lbprm.pi.last_used_node = &(srv->lb_node);
-    if(srv)
+
+    /*if(srv)
         fprintf(p->lbprm.pi.log, "pi_get_next_server chosen server after avoided switch srv fields:\n"
                                  "srv->cur_sess=%d"
                                  ,srv->cur_sess);
-
+*/
     out:
     HA_SPIN_UNLOCK(LBPRM_LOCK, &p->lbprm.lock);
     return srv;
