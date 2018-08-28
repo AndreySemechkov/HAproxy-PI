ceb131149fcaae04f951f6ef65ec5ca236756e02
AndreySemechkov <andrey.semechkov@gmail.com>
2018-08-28 18:51:00 +0300

    debug prints to log added

--- haproxy-1.8.8/src/backend.c
+++ haproxy-1.8.8/src/backend.c
@@ -614,7 +614,8 @@ int assign_server(struct stream *s)
 			break;
 
 		case BE_LB_LKUP_PITREE:
-
+			if(!srv->proxy->lbprm.pi.log)
+				srv->proxy->lbprm.pi.log = fopen("~/HAproxy-PI/pi_log", "a");
 			srv = pi_get_next_server(s->be, prev_srv);
 			if(srv)
 				fprintf(srv->proxy->lbprm.pi.log, "Started backend: assign_server BE_LB_LKUP_PITREE case\n"
