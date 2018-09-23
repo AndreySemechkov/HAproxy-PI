82daf8880026d3991e35fb43f47e73d3230bb66f
JenyaKapul <37771786+JenyaKapul@users.noreply.github.com>
2018-09-24 01:48:41 +0300

    Client ran for the simulations

--- WebService/newClient.py
+++ WebService/newClient.py
@@ -5,13 +5,14 @@ import grequests
 import requests
 import json
 import threading
+import sys
 
-
-NUM_OF_THREADS = 5
-REQ_PER_THREAD = 10
+NUM_OF_THREADS = 50
+REQ_PER_THREAD = 1000
 
 threadsList = []
 response_list = [None] * (NUM_OF_THREADS * REQ_PER_THREAD)
+
 
 def genFixedDigitsRand(n):
     range_start = 10 ** (n - 1)
@@ -22,13 +23,13 @@ def genFixedDigitsRand(n):
 def myThread(id):
     for i in range(REQ_PER_THREAD):
         httpGET(genFixedDigitsRand(5))
-        response_list[id*REQ_PER_THREAD + i] = httpGET(genFixedDigitsRand(5))
+        response_list[id * REQ_PER_THREAD + i] = httpGET(genFixedDigitsRand(5))
 
 
 def initializeWorkers():
     print "Creating threads..."
     for t_id in range(NUM_OF_THREADS):
-        threadsList.append(threading.Thread(target=myThread, kwargs={"id":t_id}))
+        threadsList.append(threading.Thread(target=myThread, kwargs={"id": t_id}))
     print "Threads were created successfully!\n"
 
     print "Starting threads..."
@@ -51,60 +52,34 @@ def httpGET(randNum):
     return request
 
 
-
 start = time.time()
 print "Executing client ..."
 
 initializeWorkers()
 joinAllThreads()
 
+totalTime = time.time() - start
+
 print "*******************"
-
-
-csv = open('t.csv', 'w')
-for key, value in json.loads(response_list[0].text).iteritems():
-    csv.write(key + ", ")
 
 errors = 0
 
 for response in response_list:
-    csv.write("\n")
     if response.status_code != 200:
         errors += 1
-        continue
-    for key, value in json.loads(response.text).iteritems():
-        csv.write(str(value) + ", ", )
+
+fileName = 'PI' + str(sys.argv[1]) + '_T' + str(totalTime) + '_E' + str(errors) + '.csv'
+csv = open(fileName, 'w')
+
+for key, value in json.loads(response_list[0].text).iteritems():
+    csv.write(key + ", ")
+
+for response in response_list:
+    if response.status_code == 200:
+        csv.write("\n")
+        for key, value in json.loads(response.text).iteritems():
+            csv.write(str(value) + ", ", )
+
 csv.close()
 print "Total errors: " + str(errors)
-
-
-
-# suc = []
-# fail = []
-# for response in response_list:
-#     if response.status_code == 200:
-#         suc.append(response.content)
-#     else:
-#         fail.append(response.content)
-
-# d = list(map(lambda s: json.loads(s), suc))
-# s = sorted(d, key=lambda k: k['timeRequestReceived'])
-# f = list(map(lambda j: json.dumps(j), s))
-
-# csv = open('t.csv', 'a')
-# for key, value in s[0].iteritems():
-#     csv.write(key + ", ")
-#
-# for d in s:
-#     csv.write("\n")
-#     for key, value in d.iteritems():
-#         csv.write(str(value) + ", ", )
-# csv.close()
-#
-# print "Timed out requests count: " + str(len(fail))
-# for f in fail:
-#     print f
-
-
-print
-print(time.time() - start)
+print "Total time: " + str(totalTime)
